6c97ac2680f95fadefef592a67740f2530c41199
Cαιɳ <36238059+gothboiclique@users.noreply.github.com>
2019-11-11 22:41:46 +0000

    Create slots-bot-tm.js
    Just add the script to TamperMonkey and enable scripts. Go to the slots page and it'll start.

---
+++ slots-bot-tm.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name         Hackforum's Dice Bot by Joel
+// @namespace    https://github.com/gothboiclique/Hackforums-Slots-Bot/
+// @version      0.1
+// @description  Get that dice award bro.
+// @author       https://hackforums.net/member.php?action=profile&uid=3790579
+// @match        https://hackforums.net/slots.php
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var starterBytes = document.getElementById("credits").innerHTML;
+    var counter = 1;
+    var bytesProfit = 0;
+    var safetySwitchAmount = 0; // 0 = Infinite, Anything else is what it will stop at :)
+    soundManager.mute();
+
+    const sleep = (milliseconds) => {
+        return new Promise(resolve => setTimeout(resolve, milliseconds))
+    }
+
+    function botSpins() {
+        document.getElementById("spinButton").click();
+    }
+
+    function safetySwitch() {
+        if (parseInt(document.getElementById("credits").innerHTML, 10) == safetySwitchAmount) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function printStats() {
+        bytesProfit = document.getElementById("credits").innerHTML - starterBytes;
+        console.log("~~~~~~~~~~~~~~~~~~~~");
+        console.log("Starting Bytes: " + starterBytes);
+        console.log("Current Bytes: " + document.getElementById("credits").innerHTML);
+        console.log("Current Profit: " + bytesProfit);
+        console.log("You have played " + counter++ + " times.");
+        console.log("Safety Switch Amount: " + safetySwitchAmount)
+        console.log("~~~~~~~~~~~~~~~~~~~~");
+    }
+
+    function doEverything() {
+        if (safetySwitch()) {
+            botSpins();
+            printStats();
+        }
+    }
+
+    function runBot() {
+        sleep(4000).then(() => {
+            if (document.getElementById("credits").innerHTML >= 1) {
+                doEverything();
+                runBot();
+            }
+        })
+    }
+
+    runBot();
+})();
